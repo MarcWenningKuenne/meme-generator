10919137f51b35ec2279b2a1de5b9b7bd43d7ee2
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-06-24 22:26:26 +0200

    17 Add images to the Meme Generator - state for the ImageURL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Meme from "./components/Meme";
 import "./style.css"
 
+
 function App() {
   return (
     <div className="App">
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,26 @@
 import React from "react"
+import memesData from "../memesData";
 
 function Meme() {
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function changeMeme(){
+        let memes = memesData.data.memes
+        let memeID = Math.floor(Math.random() * memesData.data.memes.length)
+        let imageURL = memes[memeID].url
+        setMemeImage(imageURL)
+    }
+
     return (
         <main>
             <form>
                 <div>
-                    <input className="input1" type="text" placeholder="Top text"/>
+                    <input onClick={changeMeme} className="input1" type="text" placeholder="Top text"/>
                     <input className="input2" type="text" placeholder="Bottom text"/>
                 </div>
-                <button>Get a new meme image 🖼</button>
             </form>
-            <img src=""/>
+            <button onClick={changeMeme}>Get a new meme image 🖼</button>
+            <img src={memeImage}/>
         </main>
     )
 }
